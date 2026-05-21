4f35f4b85856b1033c99714be54da299a36511be
Hok Chiu Shen <hokchiu.shen@mail.utoronto.ca>
2026-05-21 19:20:06 -0400

    Revert "changes to JSONTranslation and ByHand"

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -23,10 +23,7 @@ public class JSONTranslationExample {
             // which we then create a new JSONArray object from.
             // TODO CheckStyle: Line is longer than 120 characters
             //                  (note: you can split a line such that the next line starts with a .method()... call
-            String jsonString = Files.readString(
-                    Paths.get(getClass().getClassLoader()
-                            .getResource("sample.json")
-                            .toURI()));
+            String jsonString = Files.readString(Paths.get(getClass().getClassLoader().getResource("sample.json").toURI()));
             this.jsonArray = new JSONArray(jsonString);
         }
         catch (IOException | URISyntaxException ex) {
@@ -39,7 +36,9 @@ public class JSONTranslationExample {
      * @return the Spanish translation of Canada
      */
     public String getCanadaCountryNameSpanishTranslation() {
-        JSONObject canada = jsonArray.getJSONObject(CANADA_INDEX);
+
+        // TODO Checkstyle: '30' is a magic number.
+        JSONObject canada = jsonArray.getJSONObject(30);
         return canada.getString("es");
     }
 
